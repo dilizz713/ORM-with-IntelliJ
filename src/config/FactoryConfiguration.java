6954b9070d751e738c0c26edbc760f2c462535de
dilini <dilinichamikasilva@gmail.com>
2025-02-19 23:32:22 +0530

    add all

---
+++ src/config/FactoryConfiguration.java
@@ -0,0 +1,30 @@
+package config;
+
+import entity.Customer;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class FactoryConfiguration {
+    private static  FactoryConfiguration factoryConfiguration;
+    private SessionFactory sessionFactory;
+
+    private FactoryConfiguration() {
+        Configuration configuration = new Configuration().configure();
+        configuration.addAnnotatedClass(Customer.class);
+        // you should add all classes like this
+
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+    public static FactoryConfiguration getInstance() {
+        if (factoryConfiguration == null) {
+            factoryConfiguration = new FactoryConfiguration();
+        }
+        return factoryConfiguration;
+    }
+
+    public Session getSession() {
+        return sessionFactory.openSession();
+    }
+}
